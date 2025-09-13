be338b2b929978ecd7da2d7d7e2d2810ba49f59b
GabaGabo974 <h.pinterest974@gmail.com>
2025-09-13 15:16:15 +0200

    Modification de la structure du git

---
+++ GLPI/GLPI_debian13.sh
@@ -0,0 +1,139 @@
+echo "1. Installation complete (LAMP & GLPI)"
+echo "2. Installation de MariaDB"
+echo "3. Installation de GLPI"
+
+read -p "De quoi avez vous besoin ? : " choix
+
+if [ "$choix" = "1" ]; then
+    #Installation des paquets
+    sudo apt update -y -qq
+    sudo add-apt-repository ppa:ondrej/php -y
+    sudo apt upgrade -y -qq
+
+    sudo apt install -y apache2 mariadb-server libapache2-mod-php8.4 \
+    php8.4 php8.4-cli php8.4-common php8.4-curl php8.4-gd \
+    php8.4-ldap php8.4-mysql php8.4-xml php8.4-mbstring php8.4-bcmath \
+    php8.4-intl php8.4-zip php8.4-bz2 php8.4-soap
+  
+    systemctl restart apache2
+    #Mariadb
+    mysql_secure_installation <<EOF
+    y
+    root
+    root
+    y
+    y
+    y
+    y
+EOF
+    hote=localhost
+    read -p "Entre le nom de la base de données: " bdd
+    read -p "Entre l'utilisateur : " utilisateur
+    read -p "Entre le mot de passe de l'utilisateur: " mdp
+
+    mysql -u root -p'root' -e "CREATE DATABASE $bdd;"
+    mysql -u root -p'root' -e "CREATE USER IF NOT EXISTS '$utilisateur'@'$hote' IDENTIFIED BY '$mdp';"
+    mysql -u root -p'root' -e "GRANT ALL PRIVILEGES ON $bdd.* TO '$utilisateur'@'$hote';"
+    mysql -u root -p'root' -e "FLUSH PRIVILEGES"
+
+    #Installation & décompression de GLPI
+
+    cd /var/www/html
+    wget https://github.com/glpi-project/glpi/releases/download/10.0.19/glpi-10.0.19.tgz
+    tar -xzf glpi-10.0.19.tgz
+    chown -R www-data:www-data /var/www/html/glpi/files
+    chmod -R 777 /var/www/html/glpi/files
+    rm glpi-10.0.19.tgz -Rf
+    systemctl restart apache2
+
+
+    # [GLPI] Montage de la base de données
+    php /var/www/html/glpi/bin/console db:install \
+    -H $hote \
+    -d $bdd \
+    -u $utilisateur \
+    -p $mdp \
+    -L fr_FR \
+    --no-interaction
+    chown -R www-data:www-data /var/www/html/glpi/files/_log
+    #cd /var/www/html/glpi/
+    #php bin/console system:check_requirements
+    adrip=$(hostname -I | cut -c1-12)
+    echo "Installation terminée, maintenant sur http://$adrip/glpi !"
+
+elif [ "$choix" = 2 ]; then
+    apt install -y apache2 mariadb-server
+    mysql_secure_installation <<EOF
+    y
+    root
+    root
+    y
+    y
+    y
+    y
+EOF
+    sudo sed -i 's/^bind-address            = 127\.0\.0\.1$/bind-address            = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+    systemctl restart mariadb
+    
+    read -p "Souhaitez-vous créer un utilisateur ? [o/n] " choix
+    if [ "$choix" = "o" ]; then
+        read -p "Entrez le nom de l'utilisateur : " user
+        read -p "Entrez le mot de passe de l'utilisateur : " mdp
+        read -p "Entre le nom ou l'adresse IP de l'hôte de l'utilisateur: " host
+        read -p "Entrez le nom de la base de donnee : " bdd
+
+        mysql -u root -p'root' -e "CREATE DATABASE IF NOT EXISTS $bdd;"
+        echo "Base de données $bdd créée"
+
+        mysql -u root -p'root' -e "CREATE USER IF NOT EXISTS '$user'@'$host' IDENTIFIED BY '$mdp';"
+        echo "Utilisateur $user créé"
+
+        mysql -u root -p'root' -e "GRANT ALL PRIVILEGES ON $bdd.* TO '$user'@'$host';"
+        mysql -u root -p'root' -e "FLUSH PRIVILEGES"
+    else
+        echo "Installation terminée"
+    fi
+elif [ "$choix" = 3 ]; then
+    #Installation des paquets
+    sudo apt update -y
+    sudo apt install -y software-properties-common
+    sudo add-apt-repository ppa:ondrej/php -y
+    sudo apt update
+
+    sudo apt install -y apache2 mariadb-server libapache2-mod-php8.4 \
+    php8.4 php8.4-cli php8.4-common php8.4-curl php8.4-gd \
+    php8.4-ldap php8.4-mysql php8.4-xml php8.4-mbstring php8.4-bcmath \
+    php8.4-intl php8.4-zip php8.4-bz2 php8.4-soap
+
+
+    apt update -y && apt upgrade -y
+    systemctl restart apache2
+
+    cd /var/www/html
+    wget https://github.com/glpi-project/glpi/releases/download/10.0.19/glpi-10.0.19.tgz
+    tar -xzvf glpi-10.0.19.tgz
+    chown -R www-data:www-data /var/www/html/glpi/
+    chmod -R 777 /var/www/html/glpi/
+    rm glpi-10.0.19.tgz -Rf
+    systemctl restart apache2
+
+    read -p "Entrez l'adresse IP de l'hôte de la base de donnée : " hote
+    read -p "Entrez le nom de la base de données: " bdd
+    read -p "Entrez le nom de l'utilisateur : " utilisateur
+    read -p "Entrez le mot de passe de l'utilisateur: " mdp
+
+
+    # [GLPI] Montage de la base de données
+    php /var/www/html/glpi/bin/console db:install \
+    -H $hote \
+    -d $bdd \
+    -u $utilisateur \
+    -p $mdp \
+    -L fr_FR \
+    --no-interaction
+    chown -R www-data:www-data /var/www/html/glpi/files/_log
+    #cd /var/www/html/glpi/
+    #php bin/console system:check_requirements
+    echo "Installation terminée, maintenant sur http://$hote/glpi !"
+fi
