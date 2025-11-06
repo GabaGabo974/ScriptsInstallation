042c5674a6022ed87ae44833425bf71d96959202
GabaGabo974 <h.pinterest974@gmail.com>
2025-11-06 16:42:34 +0100

    Associations des 3 scripts (correctement)

--- FOG/InstallationFOG.sh
+++ FOG/InstallationFOG.sh
@@ -5,7 +5,7 @@ set -e
 echo " Mise à jour du système..."
 apt update && apt upgrade -y
 
-echo " Installation des paquets nécessaires pour FOG..."
+echo " Installation des paquets nécessaires pour FOG... "
 apt install -y \
   git apache2 php php-mysql php-cli php-curl php-gd php-mbstring \
   php-xml php-json php-bcmath php-intl \
@@ -13,7 +13,7 @@ apt install -y \
   curl net-tools wget tar gzip xz-utils \
   nfs-kernel-server tftpd-hpa syslinux pxelinux
 
-echo " Préparation de Debian 12 terminée."
+echo "Préparation de Debian 12 terminée."
 
 set -e
 
@@ -59,27 +59,3 @@ chmod +x ~/fog/3_installer_fog_auto.sh
 
 echo " Téléchargement et génération des fichiers terminés."
 
-set -e
-
-cd ~/fog/fogproject/bin || { echo " Dossier fogproject introuvable."; exit 1; }
-
-echo " Lancement de l'installation automatique de FOG..."
-
-sudo bash installfog.sh <<EOL
-y
-2
-
-y
-n
-
-n
-n
-
-y
-y
-n
-n
-y
-EOL
-
-echo " Installation automatisée de FOG terminée."
